843a9f70029fd2d37b70714bb4b58fc07be705bc
fyang <check321@aliyun.com>
2017-04-19 18:13:38 +0800

    add images downloader

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -12,3 +12,10 @@ class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class JobboleArticleItem(scrapy.Item):
+    article_title = scrapy.Field()
+    article_date = scrapy.Field()
+    article_tags = scrapy.Field()
+    vote_num = scrapy.Field()
+    cover_url = scrapy.Field()
--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -3,6 +3,8 @@ import scrapy
 from scrapy.http import Request
 from urllib import parse
 
+from spider.ArticleSpider.items import JobboleArticleItem
+
 
 class JobboleSpider(scrapy.Spider):
     name = "jobbole"
@@ -10,9 +12,12 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        req_urls = response.css('#archive .floated-thumb .post-meta a::attr(href)').extract()
-        for req_url in req_urls:
-            yield Request(url= parse.urljoin(response.url,req_url) , callback=self.parse_detail)
+        # req_urls = response.css('#archive .floated-thumb .post-meta a::attr(href)').extract()
+        req_nodes = response.css('#archive .floated-thumb')
+        for req_node in req_nodes:
+            req_url = req_node.css('.post-meta a::attr(href)').extract_first()
+            img_url = req_node.css('.post-thumb a img::attr(src)').extract_first()
+            yield Request(url= parse.urljoin(response.url,req_url) ,meta={"cover_url":img_url},callback=self.parse_detail)
         next_url = response.css('.next.page-numbers::attr(href)').extract_first()
         if next_url:
             yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
@@ -26,6 +31,14 @@ class JobboleSpider(scrapy.Spider):
         # tags
         article_tags = response.css('.entry-meta-hide-on-mobile a::text').extract()
         vote_num = int(response.css('.vote-post-up h10::text').extract_first('0'))
+        cover_url = response.meta.get("cover_url")
         print("article_title:", article_title, "article_date:", article_date, "article_tags:", article_tags,
-              "vote_num:", vote_num)
-        pass
+              "vote_num:", vote_num,"cover_url:",cover_url)
+        article_item = JobboleArticleItem()
+        article_item["article_title"] = article_title
+        article_item["article_date"] = article_date
+        article_item["article_tags"] = article_tags
+        article_item["vote_num"] = vote_num
+        article_item["cover_url"] = [cover_url]
+
+        yield article_item
